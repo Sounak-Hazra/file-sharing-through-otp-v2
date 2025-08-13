617e977b2a458f2e0f80c9a7e7d7d09cdd7a040f
Sounak-Hazra <hazrasounak87@gmail.com>
2025-08-13 19:27:26 +0530

    initial

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,30 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import path, { dirname } from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname,"frontend/dist")))
 
 import uploadSingle from "./routes/upload.route.js"
 app.use("/api/v1/upload",uploadSingle)
 
 import getData from "./routes/get.route.js"
-app.use("/api/v1/getdata",getData)
+app.use("/api/v1/getdata", getData)
+
+app.all('/{*any}', (req, res) => {
+  res.sendFile(path.join(__dirname,"frontend/dist/index.html"));
+});
+
+
 export default app
